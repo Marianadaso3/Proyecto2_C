9ebdd94a2fa6e254d3e7546192c4c86f648823a5
Marianadaso3 <72479030+Marianadaso3@users.noreply.github.com>
2022-11-22 15:00:15 -0600

    Add files via upload
    Resultados correctos

--- functions.py
+++ functions.py
@@ -8,45 +8,49 @@
 #Funciones labda: https://borjauria.es/que-son-y-como-utilizar-lambdas-en-python-4d1d168e2f90
 #Funciones labda: https://realpython.com/python-lambda/#appropriate-uses-of-lambda-expressions
 #Funciones labda: https://recursospython.com/guias-y-manuales/funciones-lambda/
-#Funciones beta y alfa: https://www.toppr.com/ask/question/displaystyle-alpha-beta-are-the-root-of-the-equation-displaystyle-lambda-left-x2x/
-#Funciones beta y alfa: https://www.doubtnut.com/question-answer/alpha-beta-are-roots-of-lambda-x2-x-x-5-0-if-lambda1-and-lambda-2-are-the-two-values-of-lambda-for-w-6777
+#Funciones beta y betaop_beta: https://www.toppr.com/ask/question/displaystyle-alpha-beta-are-the-root-of-the-equation-displaystyle-lambda-left-x2x/
+#Funciones beta y betaop_beta: https://www.doubtnut.com/question-answer/alpha-beta-are-roots-of-lambda-x2-x-x-5-0-if-lambda1-and-lambda-2-are-the-two-values-of-lambda-for-w-6777
 
 #-----------------------------------Inicio de funciones----------------------------
 
 #Definimos expresiones numericas con lambda 
-#a
-cero = lambda f: lambda x: x
-#b
-uno = lambda f: lambda x: f(x)
-#c
-dos =  lambda f: lambda x: f(f(x))
-#d
-tres   = lambda f: lambda x: f(f(f(x)))
-
+cero = lambda f: lambda x: x #a
+uno = lambda f: lambda x: f(x)  #b
+dos =  lambda f: lambda x: f(f(x)) #c
+tres   = lambda f: lambda x: f(f(f(x))) #d
+# Extras cuatro y cinco 
+cuatro  = lambda f: lambda x: f(f(f(f(x))))
+cinco  = lambda f: lambda x: f(f(f(f(f(x)))))
 
 #Definimos procesos numericas con lambda 
-#e
-op_sucesor = lambda n: lambda f: lambda x: f(n(f)(x))
-#f
-op_suma = lambda a: lambda b: lambda f: lambda x: a(f)(b(f)(x))
-#g
-op_multiplicacion = lambda a: lambda b: lambda f: lambda x: a(b(f))(x)
-#h
-op_potencia = lambda a: lambda b: lambda f: lambda x: b(a)(f)(x)
+op_sucesor = lambda n: lambda f: lambda x: f(n(f)(x)) #e
+op_suma = lambda a: lambda b: lambda f: lambda x: a(f)(b(f)(x)) #f
+op_multiplicacion = lambda a: lambda b: lambda f: lambda x: a(b(f))(x) #g
+op_potencia = lambda a: lambda b: lambda f: lambda x: b(a)(f)(x) #h
 
-#Operaciones lamda con op_alfa y beta 
-op_alfa = lambda x: x+1
+#Operaciones lamda con op_beta y beta 
+op_alfa = lambda x: x + 1
 op_beta = lambda x: 2 * x
 
 #Funciones para ejemplificar 
+def valores_num():
+    resultado_cero = cero(op_beta)(5) #probar con beta
+    resultado_uno = uno(op_beta)(5)
+    resultado_dos = dos(op_beta)(5)
+    resultado_tres = tres(op_beta)(5)
+    resultado_cuatro = cuatro(op_beta)(5)
+    resultado_cinco = cinco(op_beta)(5)
+    print ("Los resultados de los valores respectivamente del cero al cinco son: ", "\nCERO:",resultado_cero,"\nUNO:", resultado_uno, "\nDOS:",resultado_dos,"\nTRES:",resultado_tres,"\nCUATRO:",resultado_cuatro,"\nCINCO:",resultado_cinco)
+
 def sucesor():
-    print ("Valor que le sigue a 1 es: ", op_sucesor(uno)(op_alfa)(cero(op_alfa)(0)))  
-
+    sol_sucesor = (op_sucesor(dos)(op_beta)(5))
+    print ("Valor que le sigue es: ", sol_sucesor)  
 def suma():
-    print ("La suma de 2 + 2 es:", op_suma(dos)(dos)(op_alfa)(cero(op_alfa)(0)))
-
+    sol_suma = (op_suma(dos)(tres)(op_beta)(5))
+    print ("El reultado de la suma es: ", sol_suma)
 def multiplicacion():
-    print ("El factor de 2 * 3 es:", op_multiplicacion(dos)(tres)(op_alfa)(cero(op_alfa)(0)))  
-
+    sol_multiplicación = (op_multiplicacion(dos)(tres)(op_beta)(5))
+    print ("El resultado de la multiplicación es: ", sol_multiplicación)  
 def potencia ():
-    print ("La potencia del numero 3 es: ", op_potencia(tres)(tres)(op_alfa)(cero(op_alfa)(0)))
+    sol_potencia = (op_potencia(dos)(tres)(op_beta)(5))
+    print ("El resultado de la potencia es: ", sol_potencia)
--- main.py
+++ main.py
@@ -15,25 +15,28 @@ def menu():
     #Funcion que limpia pantalla
     #os.system('cls')
     print ("\n Seleccione la opción del ejercicio que desea ejecutar")
-    print ("1. Ejemplo con sucesor")
-    print ("2. Ejemplo con suma")
-    print ("3. Ejemplo con multiplicación")
-    print ("4. Ejemplo con potencia")
-    print ("5. Salir")
+    print ("1. Resultado de los valores del cero al cinco")
+    print ("2. Ejemplo con sucesor")
+    print ("3. Ejemplo con suma")
+    print ("4. Ejemplo con multiplicación")
+    print ("5. Ejemplo con potencia")
+    print ("6. Salir")
 
 while True: 
     menu()
 
     opcionMenu = input("\nInserte un valor numérico para seleccionar la opcion: ")
     if opcionMenu == "1":
+        resultado = functions.valores_num()
+    if opcionMenu == "2":
         resultado = functions.sucesor()
-    elif opcionMenu == "2":
-        resultado = functions.suma()
     elif opcionMenu == "3":
-        resultado = functions.multiplicacion()
+        resultado = functions.suma()
     elif opcionMenu == "4":
-        resultado = functions.potencia()
+        resultado = functions.multiplicacion()
     elif opcionMenu == "5":
+        resultado = functions.potencia()
+    elif opcionMenu == "6":
         print("¡Usted ha salido exitosamente!")
         break
     else:
